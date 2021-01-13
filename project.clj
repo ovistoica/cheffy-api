4b7f074a63fef8c2c38568b545e5b46064c47600
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-01-13 12:45:16 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject cheffy "0.1.0-SNAPSHOT"
+  :description "Cheffy rest API"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.1"]
+                 [integrant "0.8.0"]
+                 [environ "1.2.0"]
+                 [metosin/reitit "0.5.11"]
+                 [seancorfield/next.jdbc "1.1.613"]
+                 [org.postgresql/postgresql "42.2.18"]
+                 [clj-http "3.10.0"]
+                 [ovotech/ring-jwt "2.2.1"]]
+  :profiles {:uberjar {:aot      :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev     {
+                       :source-paths   ["dev/src"]
+                       :resource-paths ["dev/resources"]
+                       :dependencies   [[ring/ring-mock "0.4.0"] [integrant/repl "0.3.2"]]
+                       }}
+  :uberjar-name "cheffy.jar")
