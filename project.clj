2c09e2d8363105a24cb816537f27ff2bc20b3e78
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-01-13 12:46:49 +0200

    Add min leiningen version 2.0.0

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject cheffy "0.1.0-SNAPSHOT"
   :description "Cheffy rest API"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.1"]
                  [integrant "0.8.0"]
